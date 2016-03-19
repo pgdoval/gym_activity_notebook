6a167dbb2b2a0a8fc4fd1249c9bdd9a6d760e20f
Pablo González Doval <dovaleac@gmail.com>
2016-03-19 16:50:06 +0100

    Inicio del gestor de actividades del gimnasio

---
+++ lib/components/activities_list_component.dart
@@ -0,0 +1,7 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/angular2.dart';
+
+@Component(selector: 'my-app', templateUrl: 'activities_list_component.html')
+class ActivitiesListComponent {}
